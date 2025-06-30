cc2b600953a6bca6954d6387cc255649e594437e
Lau02x <castrorodriguez2@gmail.com>
2025-06-29 22:14:59 -0300

    primer ejercicio completado

--- Tareas/Program.cs
+++ Tareas/Program.cs
@@ -2,41 +2,54 @@
 using System.Resources;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-
+using Tareas;
 HttpClient client = new HttpClient();
 HttpResponseMessage response = await client.GetAsync("https://jsonplaceholder.typicode.com/todos");
 response.EnsureSuccessStatusCode();
 string responseBody = await response.Content.ReadAsStringAsync();
-List<Tarea> lista_tareas = JsonSerializer.Deserialize<List<Tarea>>(responseBody);
+List<Tarea> lista_tareas = JsonSerializer.Deserialize<List<Tarea>>(responseBody) ?? new List<Tarea>();
+List<Tarea> lista_tareas_ordenada = new List<Tarea>();
+string path_build = Directory.GetCurrentDirectory();
+string path_relativo = Directory.GetParent(path_build).Parent.Parent.FullName;
+string path_archivoJson = path_relativo + "\\tareas.json";
+
 System.Console.WriteLine("-----Pendientes: -----\n");
 foreach (var tarea in lista_tareas)
 {
-    if (!tarea.completed)
+    if (!tarea.Completed)
     {
-        Console.WriteLine("\nTitulo:" + tarea.title + "\ncompletado:" + tarea.completed);
+        Console.WriteLine("\nTitulo:" + tarea.Title + "\ncompletado:" + tarea.Completed);
+        lista_tareas_ordenada.Add(tarea);
     }
 }
 
 System.Console.WriteLine("-----Completados: -----\n");
 foreach (var tarea in lista_tareas)
 {
-    if (tarea.completed)
+    if (tarea.Completed)
     {
-        Console.WriteLine("\nTitulo:" + tarea.title + "\ncompletado:" + tarea.completed);
+        Console.WriteLine("\nTitulo:" + tarea.Title + "\ncompletado:" + tarea.Completed);
+        lista_tareas_ordenada.Add(tarea);
     }
 }
-string jsonString = JsonSerializer.Serialize(lista_tareas);
+string jsonString = JsonSerializer.Serialize(lista_tareas_ordenada);
+File.WriteAllText(path_archivoJson,jsonString);
 
 
 
-
-
+namespace Tareas
+{
 public class Tarea
 {
-    public int userId { get; set; }
-    public int id { get; set; }
-    public string title { get; set; }
-    public bool completed { get; set; }
+    [JsonPropertyName("userId")]
+    public int UserId { get; set; }
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+    [JsonPropertyName("title")]
+    public required string Title { get; set; }
+    [JsonPropertyName("completed")]
+    public bool Completed { get; set; }
+}
 }
 
 
